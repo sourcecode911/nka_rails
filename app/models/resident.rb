351bed3a117e4928e1c943706e43b66b96282296
Cedric Oswald <cedric@oswaldonline.info>
2017-06-20 17:44:25 +0200

    Added some resident logic for partial invoices

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -1,4 +1,40 @@
 class Resident < ApplicationRecord
   belongs_to :flat
   belongs_to :user
+
+  scope :by_user, lambda { |user_id| where(user_id: user_id) }
+  scope :current, lambda { |user_id| by_user(user_id).where(move_out: nil) }
+  scope :of_year, lambda { |user_id, year| by_user(user_id).where("move_out >= ?", Date.new(year, 1, 1)).or(by_user(user_id).where(move_out: nil)) }
+
+
+  def year_share(year)
+    first_day = Date.new(year, 1, 1)
+    last_day = Date.new(year, 12, 31)
+    if(move_in < first_day)
+      start = first_day
+    elsif move_in > last_day
+      return 0
+    else
+      start = move_in
+    end
+
+    if(move_out.nil? || move_out > last_day)
+      ende = last_day
+    elsif move_out < first_day
+      return 0
+    else
+      ende = move_out
+    end
+    byebug
+    
+    return ((ende - start) / (leap?(year) ? 366 : 365).to_f)
+  end
+
+  def leap?(year)
+    if (year % 400) == 0 || ((year % 4) == 0 && (year % 100) != 0)
+      return true;
+    end
+
+    false
+  end
 end
