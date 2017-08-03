55355e8b2d39ae62b878ed3a9a804bcf129694a4
Cedric Oswald <c.oswald@eurodata.de>
2017-08-03 15:36:22 +0200

    Added additional test_user and test_data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,28 @@
 
 Invoice.delete_all
 User.delete_all
-User.create(username: 'Cedric Oswald', email: 'cedric@oswaldonline.info', password: 'admin0815')
-User.create(username: 'Sabine Oswald', email: 'sabine@oswaldonline.de', password: 'vds12%z6z/&hn!')
+Meter.delete_all
+Count.delete_all
+Flat.delete_all
+InvoiceDetail.delete_all
+Resident.delete_all
+
+
+user1 = User.create(username: 'Cedric Oswald', email: 'cedric@oswaldonline.info', password: 'admin0815')
+user2 = User.create(username: 'Sabine Oswald', email: 'sabine@oswaldonline.de', password: 'vds12%z6z/&hn!')
+user3 = User.create(username: 'Karin Al Roubaiey', email: 'karin@test.de', password: 'karin_test')
+
+flat1 = Flat.create(name: 'Wohnung 1', user_id: user1.id, area: 50, tax: 19, ownership: 50)
+flat2 = Flat.create(name: 'Wohnung 2', user_id: user1.id, area: 50, tax: 19, ownership: 50)
+
+Meter.create(name: 'KW1', flat_id: flat1.id, user_id: user1.id, expense_type: 0)
+Meter.create(name: 'WW1', flat_id: flat1.id, user_id: user1.id, expense_type: 1)
+Meter.create(name: 'H1', flat_id: flat1.id, user_id: user1.id, expense_type: 2)
+Meter.create(name: 'S1', flat_id: flat1.id, user_id: user1.id, expense_type: 3)
+Meter.create(name: 'WWG1', flat_id: flat1.id, user_id: user1.id, expense_type: 5)
+
+Meter.create(name: 'KW2', flat_id: flat2.id, user_id: user1.id, expense_type: 0)
+Meter.create(name: 'WW2', flat_id: flat2.id, user_id: user1.id, expense_type: 1)
+Meter.create(name: 'H2', flat_id: flat2.id, user_id: user1.id, expense_type: 2)
+Meter.create(name: 'S2', flat_id: flat2.id, user_id: user1.id, expense_type: 3)
+Meter.create(name: 'WWG2', flat_id: flat2.id, user_id: user1.id, expense_type: 5)
